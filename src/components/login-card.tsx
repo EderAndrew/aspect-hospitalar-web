2495949babb8a7d619b0d061a9eee2719d76ed28
Eder Andrew <ederandrew0028@gmail.com>
2026-01-26 23:16:29 -0300

    First commit

---
+++ src/components/login-card.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CalendarCheck, Lock, Mail } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+import { Field, FieldError, FieldGroup, FieldLabel } from "./ui/field";
+import { useState } from "react";
+
+const loginFormSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6, "Senha deve ter ao menos 6 caracteres."),
+});
+
+export const LoginCard = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof loginFormSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Card className="w-full max-w-md shadow-xl">
+      <CardHeader className="space-y-4 text-center">
+        <div className="mx-auto w-16 h-16 bg-primary rounded-full flex items-center justify-center">
+          <CalendarCheck className="size-8 text-primary-foreground" />
+        </div>
+        <div>
+          <CardTitle className="text-3xl">Bem-vindo</CardTitle>
+          <CardDescription className="text-base mt-2">
+            Sistema de Agendamento Hospitalar
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FieldGroup>
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-email">Email</FieldLabel>
+                  <div className="relative">
+                    <Mail className="absolute left-3 top-3 size-4 text-muted-foreground" />
+                    <Input
+                      {...field}
+                      id="form-rhf-email"
+                      type="email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="seu.email@exemplo.com"
+                      autoComplete="on"
+                      className="pl-10"
+                    />
+                  </div>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-pwd">Senha</FieldLabel>
+                  <div className="relative">
+                    <Lock className="absolute left-3 top-3 size-4 text-muted-foreground" />
+                    <Input
+                      {...field}
+                      id="form-rhf-pwd"
+                      type="password"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="••••••••"
+                      autoComplete="on"
+                      className="pl-10"
+                    />
+                  </div>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+          <Button
+            type="submit"
+            className="w-full mt-6"
+            size="lg"
+            disabled={isLoading}
+          >
+            {isLoading ? "Entrando..." : "Entrar"}
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
